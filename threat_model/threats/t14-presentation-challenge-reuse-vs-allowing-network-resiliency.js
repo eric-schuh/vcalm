9d7729f720eca75d95006f7fe10fc0b8a2bd850a
Joe Andrieu <joe@andrieu.net>
2026-06-30 12:15:25 -0700

    Update threat_model/threats/t14-presentation-challenge-reuse-vs-allowing-network-resiliency.js
    Co-authored-by: Manu Sporny <msporny@digitalbazaar.com>

--- threat_model/threats/t14-presentation-challenge-reuse-vs-allowing-network-resiliency.js
+++ threat_model/threats/t14-presentation-challenge-reuse-vs-allowing-network-resiliency.js
@@ -4,7 +4,7 @@
         name: "Presentation Challenge Reuse vs Allowing Network Resiliency",
         desc: "Reusing cryptographic challenge tokens to bridge unreliable mobile connections allows network sniffers to capture presentation payloads and execute replay attacks.",
         response: [
-            { id: "R14", name: "Domain-Bound Verification Challenges", type: "Reduce", desc: "Embed unique, strict single-use cryptographic nonces combined with target audience domain pins inside every transaction request object." }
+            { id: "R14", name: "Domain-Bound Verification Challenges", type: "Reduce", desc: "Embed unique, strict single-use cryptographic challenges combined with target audience domain pins inside  presentation request and response objects." }
         ],
         elements: ["P1", "P5", "F7", "F8", "F12", "F19"],
         taxonomyName: "STRIDE",
