dbecfea57a0a079d8d7a4e34128a58ed599d368a
Joe Andrieu <joe@andrieu.net>
2026-06-30 12:52:02 -0700

    Update threat_model/threats/t29-ecosystem-gatekeeping-in-the-name-of-security-or-privacy.js
    Co-authored-by: Manu Sporny <msporny@digitalbazaar.com>

--- threat_model/threats/t29-ecosystem-gatekeeping-in-the-name-of-security-or-privacy.js
+++ threat_model/threats/t29-ecosystem-gatekeeping-in-the-name-of-security-or-privacy.js
@@ -2,7 +2,7 @@
     var threat = {
         id: "T29",
         name: "Ecosystem Gatekeeping in the Name of Security or Privacy",
-        desc: "Primary ecosystem actors choose to restrict their support of particular protocols in the name of security or privacy whereas in reality they are trying to push the ecosystem to adopt their solution.",
+        desc: "Primary ecosystem actors choose to restrict their support of particular protocols or data formats in the name of security or privacy whereas in reality they are trying to push the ecosystem to adopt their solution.",
         response: [],
         elements: ["E1"],
         taxonomyName: "TODO",
