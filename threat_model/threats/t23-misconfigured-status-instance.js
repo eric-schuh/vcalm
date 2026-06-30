0bd802e4ab1d1939c9d63206db9e3d20820cfe1a
Joe Andrieu <joe@andrieu.net>
2026-06-30 12:29:22 -0700

    Update threat_model/threats/t23-misconfigured-status-instance.js
    Co-authored-by: Manu Sporny <msporny@digitalbazaar.com>

--- threat_model/threats/t23-misconfigured-status-instance.js
+++ threat_model/threats/t23-misconfigured-status-instance.js
@@ -4,7 +4,7 @@
         name: "Misconfigured Status Instance",
         desc: "An incorrectly deployed status service allows unauthorized users to write unvetted status indicators or read metrics without proper authorization checks.",
         response: [
-            { id: "R20", name: "Code Validation", type: "Reduce", desc: "Mandate that security configuration properties navigate automated vulnerability configuration scanning checks before hitting production environments." },
+            { id: "R20", name: "Code Validation", type: "Reduce", desc: "Mandate that security configuration properties pass automated vulnerability configuration scanning checks before hitting production environments." },
             { id: "R21", name: "Cryptographically Bound Registry Permissions", type: "Reduce", desc: "Hardcode security dependencies requiring matching cryptographic signature assertions to stop unauthorized status modifications." },
             { id: "R27", name: "Robust Testing", type: "Reduce", desc: "Each workflow and individual endpoint on all processes should have robust testing to ensure expected results." }
         ],
