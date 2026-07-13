540b5a6c0e31154c12059fe2b79fe4dd09f1ed4e
eric-schuh <eric.schuh@gmail.com>
2026-07-13 11:26:27 -0500

    Apply suggestion from @TallTed
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

--- threat_model/threats/t17-leaking-information-due-to-status-list-vc-updates.js
+++ threat_model/threats/t17-leaking-information-due-to-status-list-vc-updates.js
@@ -2,7 +2,7 @@
     var threat = {
         id: "T17",
         name: "Leaking Information Due to Status List VC Updates",
-        desc: "External observers consistently query status services to discover some information about the system issuing credentials, whether that be the rate at which they are issued, how many get issued, or in extreme cases for little used systems, who was issued which credential when.",
+        desc: "External observers consistently query status services to discover some information about the system issuing credentials, whether that be the rate at which they are issued, how many get issued, or in extreme cases for little used systems, what credential was issued to whom and when.",
         response: [
             { id: "R17", name: "Deterministic Batching Schedules and State Padding", type: "Reduce", desc: "Push state modifications to public status lists, on strict, standardized, scheduled intervals, and fill unallocated tracking bits with random placeholder configurations, instead of streaming real-time alerts." },
             { id: "R22", name: "Incorporate Fake Data", type: "Reduce", desc: "Make use of random assignment of fake credentials to a status list, at random times, to obfuscate the true number of credentials that have been assigned and the rate at which credentials are being issued." }
