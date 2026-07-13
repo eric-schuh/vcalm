f6b7c4a8c9fc0caa74b7d523c5cfd5a55d754332
eric-schuh <eric.schuh@gmail.com>
2026-07-13 11:24:30 -0500

    Apply suggestion from @TallTed
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

--- threat_model/threats/t1-dos-attacks-against-public-coordinator-endpoints.js
+++ threat_model/threats/t1-dos-attacks-against-public-coordinator-endpoints.js
@@ -2,7 +2,7 @@
     var threat = {
         id: "T1",
         name: "DoS Attacks Against Public Coordinator Endpoints",
-        desc: "Attackers perform a denial of service attack against a public coordinator endpoint(s), causing severe availability degradation for legitimate clients.",
+        desc: "Attackers perform a denial of service attack against one or more public coordinator endpoints, causing severe degradation of availability for legitimate clients.",
         response: [
             { id: "R1", name: "Volumetric Rate Limiting and Web Application Firewall Architecture", type: "Reduce", desc: "Deploy an enterprise Web Application Firewall (WAF) coupled with strict Internet Protocol (IP) and token-based rate limiting thresholds at the Application Programming Interface (API) Gateway to drop volumetric attack traffic." },
             { id: "R2", name: "Event-Driven Architecture and Async Message Queues", type: "Reduce", desc: "Decouple direct synchronous processing loops by routing transactional requests into asynchronous, managed worker queues to shield critical database threads from flooding." }
