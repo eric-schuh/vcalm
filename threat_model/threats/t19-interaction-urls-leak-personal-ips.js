8798003ec519163a9e56508450043634bd59f66a
eric-schuh <eric.schuh@gmail.com>
2026-07-13 11:33:48 -0500

    Apply suggestion from @TallTed
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

--- threat_model/threats/t19-interaction-urls-leak-personal-ips.js
+++ threat_model/threats/t19-interaction-urls-leak-personal-ips.js
@@ -3,7 +3,7 @@
         id: "T19",
         name: "Interaction URLs Leak Personal IPs",
         desc: "Direct network connections to interaction URLs expose user IP metrics and geographic data to backend servers during verification checks.",
-        response: [],
+        response: ["Use interaction URL proxies, mixnets, or interaction URL locations where the population using the interaction URLs is large and diverse."],
         elements: ["E1", "F6", "P4"],
         taxonomyName: "STRIDE",
         taxonomyClass: "Information Disclosure"
