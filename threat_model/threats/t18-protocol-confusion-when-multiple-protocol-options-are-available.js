e3b3d91b58c900af20c51411acc759c5d9c33ea3
eric-schuh <eric.schuh@gmail.com>
2026-07-13 11:30:45 -0500

    Apply suggestion from @TallTed
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

--- threat_model/threats/t18-protocol-confusion-when-multiple-protocol-options-are-available.js
+++ threat_model/threats/t18-protocol-confusion-when-multiple-protocol-options-are-available.js
@@ -4,7 +4,7 @@
         name: "Protocol Confusion When Multiple Protocol Options Are Available",
         desc: "A mixed-profile deployment environment allows an adversary to intentionally submit legacy or insecure alternative versions of requests, tricking the coordinator into lowering validation rules.",
         response: [
-            { id: "R18", name: "Interface Deprecation and Secure Fallback Disabling", type: "Reduce", desc: "Hardcode execution restrictions that block processing steps for legacy or unvetted fallback patterns, enforcing a strict protocol specification validation policy." }
+            { id: "R18", name: "Deprecating Interfaces and Disabling Secure Fallbacks", type: "Reduce", desc: "Hardcode execution restrictions that block processing steps for legacy or unvetted fallback patterns, enforcing a strict validation policy for protocol specifications." }
         ],
         elements: ["D4", "P4", "F6", "P1"],
         taxonomyName: "STRIDE",
